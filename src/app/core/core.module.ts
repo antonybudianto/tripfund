e3253de0fcd479fcdc9479c0a819a0c32f2bfb84
hendryfoe <hendryanto.fudiko@gmail.com>
2017-04-22 18:56:55 +0700

    add and register signin service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,27 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppFirebaseModule } from './firebase/app-firebase.module';
-import { NavbarModule } from './navbar/navbar.module';
 import { HeaderModule } from './header/header.module';
 import { CardModule } from './card/card.module';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthService } from './auth/auth.service';
+import { SigninService } from '../guest/signin/signin.service';
 
 @NgModule({
     imports: [
         CommonModule,
         AppFirebaseModule,
-        NavbarModule,
         HeaderModule,
         CardModule
     ],
     exports: [
-        NavbarModule,
         HeaderModule,
         CardModule
     ],
     providers: [
         AuthService,
+        SigninService,
         AuthGuard
     ]
 })
