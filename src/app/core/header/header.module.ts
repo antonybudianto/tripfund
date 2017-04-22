2b077a25f51949e751d973702bd50e2a0dc44949
hendryfoe <hendryanto.fudiko@gmail.com>
2017-04-22 19:01:02 +0700

    add login implementation

--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { HeaderComponent } from './header.component';
+import { NavbarLoginComponent } from './login/navbar-login.component';
 
 @NgModule({
     declarations: [
-        HeaderComponent
+        HeaderComponent,
+        NavbarLoginComponent
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule,
+        BsDropdownModule.forRoot()
     ],
     exports: [
         HeaderComponent
