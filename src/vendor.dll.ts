cc50cdf8a1ff0b0293ff422117dc04b714f46589
Antony Budianto <antonybudianto@gmail.com>
2017-04-23 14:50:05 +0700

    add ng animation to dll

--- src/vendor.dll.ts
+++ src/vendor.dll.ts
@@ -9,6 +9,7 @@ import '@angular/core';
 import '@angular/common';
 import '@angular/compiler';
 import '@angular/platform-browser';
+import '@angular/platform-browser/animations';
 import '@angular/platform-browser-dynamic';
 import '@angular/forms';
 import '@angular/http';
